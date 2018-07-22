55a2cf1f8c1fcf19f8010a1e128ac42fd8c6c3ba
Naji El Kotob <naji@dotnetheroes.com>
2018-07-22 09:37:29 +0300

    Add the Courses page

--- DevReadyAcademy/Migrations/Configuration.cs
+++ DevReadyAcademy/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace DevReadyAcademy.Migrations
 {
+    using DevReadyAcademy.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -21,6 +22,19 @@ namespace DevReadyAcademy.Migrations
             //  to avoid creating duplicate seed data.
 
 
+            // Use Code First Migrations to Seed the Database
+            // https://docs.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-3
+
+            context.Courses.AddOrUpdate(
+              p => p.Id,
+              new Course { Title = "Analyzing Data with Power BI" },
+              new Course { Title = "Querying Microsoft SQL Server" },
+              new Course { Title = "Administering Microsoft SQL Server Databases" },
+              new Course { Title = "Implementing a Data Warehouse with Microsoft SQL Server" },
+              new Course { Title = "Programming in C#" },
+              new Course { Title= "Developing ASP.NET MVC 5 Web Applications" }
+
+            );
 
         }
     }
---
+++ DevReadyAcademy/README.md
@@ -0,0 +1,4 @@
+﻿# DevReady Academy
+DevReady Academy - ASP.NET MVC 5
+
+
